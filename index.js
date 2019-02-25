8d5ef66690f458e015282c26aac3da3ac9cfa1ba
Enrique Montemayor <eemontemayor@live.com>
2019-02-25 14:56:49 -0600

    added toggleHideFilter func

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const STORE = [
+const STORE = {
+  items:[
   {id: cuid(), name: 'apples', checked: false},
   {id: cuid(), name: 'oranges', checked: false},
   {id: cuid(), name: 'milk', checked: true},
   {id: cuid(), name: 'bread', checked: false},
-];
+],
+  hideCompleted: false,
+};
 
 function generateItemElement(item){
   return `<li class="js-item-index-element" data-item-id="${item.id}">
@@ -22,39 +25,39 @@ function generateItemElement(item){
 }
 
 function  generateShoppingItemsString(shoppingList){
-  const items= shoppingList.map((item, index) => generateItemElement(item, index));
+  const items= shoppingList.map((item) => generateItemElement(item));
   return items.join('');
 }
 
 function renderShoppingList() {
-  console.log('rendering shopping list');
-  const shoppingListString = generateShoppingItemsString(STORE);
-
-  $('.js-shopping-list').html(shoppingListString);
-
+  let filteredItems= STORE.items;
+  if (STORE.hideCompleted){
+    filteredItems = filteredItems.filter(item => !item.checked);
+  }
+  const shoppingListItemsString = generateShoppingItemsString(filteredItems);
+  $('.js-shopping-list').html(shoppingListItemsString);
 }
 
 function addItemToShoppingList(itemName){
   console.log(`Adding "${itemName} to shopping list"`);
   if (itemName !== '') {
-    STORE.push({id: cuid(), name: itemName, checked: false});
+    STORE.items.push({id: cuid(), name: itemName, checked: false});
   }
 }
 
 function handleNewItemSubmit() {
   $('#js-shopping-list-form').submit(function(event){
     event.preventDefault();
-    const newItemName= $(`.js-shopping-list-entry`).val();
-    console.log('handling new items');
+    const newItemName= $('.js-shopping-list-entry').val();
     $('.js-shopping-list-entry').val('');
     addItemToShoppingList(newItemName);
     renderShoppingList();
   });
-  
 }
+
 function toggleCheckedForListItem(itemId) {
-  console.log(`Toggling checked property for item with with id ${itemId}.`);
-  const item = STORE.find(item => item.id === itemId);
+ 
+  const item = STORE.items.find(item => item.id === itemId);
   item.checked = !item.checked;
 }
 
@@ -62,38 +65,46 @@ function getItemIdFromElement(item) {
   return $(item).closest('li').data('item-id');
 }
 
-function deleteItem(itemId) {
-  // const item = STORE.find(item => item.id === itemId);
-  const index = STORE.findIndex(item => item.id === itemId);
-  STORE.splice(index, 1);
-}
+
 function handleItemCheckClicked() {
-  console.log('handling checked items');
   $('.js-shopping-list').on('click', '.js-item-toggle', event => {
-    console.log('item clicked');
-    const itemId = getItemIdFromElement(event.currentTarget);
-    console.log(itemId);
-    toggleCheckedForListItem(itemId);
+    const id = getItemIdFromElement(event.currentTarget);
+    toggleCheckedForListItem(id);
     renderShoppingList();
   });
+}
+
+function deleteListItem(itemId) {
+  const itemIndex = STORE.items.findIndex(item => item.id === itemId);
+  STORE.items.splice(itemIndex, 1);
 }
 
 function handleDeleteItemClicked() {
-  console.log('handling deleted items');
   $('.js-shopping-list').on('click', '.js-item-delete', event => {
     console.log('deleting item');
-    const itemId = getItemIdFromElement(event.currentTarget);
-    deleteItem(itemId);
+    const itemIndex = getItemIdFromElement(event.currentTarget);
+    deleteListItem(itemIndex);
     renderShoppingList();
   });
 
 }
+function toggleHideFilter(){
+  STORE.hideCompleted = !STORE.hideCompleted;
+}
+function handleToggleHideFilter(){
+  $('.js-hide-completed-toggle').on('click',()=>{
+    toggleHideFilter();
+    renderShoppingList();
+  });
+}
+
 
 function main() {
   renderShoppingList();
   handleNewItemSubmit();
   handleItemCheckClicked();
   handleDeleteItemClicked();
+  handleToggleHideFilter();
 }
 
 $(main);
